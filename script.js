8655166595798be76c637fb90a07e0f06795ff29
Shvets Dmytro <shvetsdmytro90@ukr.net>
2021-08-27 17:19:42 +0300

    implemented radio selection

---
+++ script.js
@@ -0,0 +1,54 @@
+// Pages
+const gamePage = document.getElementById("game-page");
+const scorePage = document.getElementById("score-page");
+const splashPage = document.getElementById("splash-page");
+const countdownPage = document.getElementById("countdown-page");
+// Splash Page
+const startForm = document.getElementById("start-form");
+const radioContainers = document.querySelectorAll(".radio-container");
+const radioInputs = document.querySelectorAll("input");
+const bestScores = document.querySelectorAll(".best-score-value");
+// Countdown Page
+const countdown = document.querySelector(".countdown");
+// Game Page
+const itemContainer = document.querySelector(".item-container");
+// Score Page
+const finalTimeEl = document.querySelector(".final-time");
+const baseTimeEl = document.querySelector(".base-time");
+const penaltyTimeEl = document.querySelector(".penalty-time");
+const playAgainBtn = document.querySelector(".play-again");
+
+// Equations
+let questionAmount = 0;
+
+// Get the value from selected radio button
+function getRadioValue() {
+  let radioValue;
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value;
+      //   break;
+    }
+  });
+  return radioValue;
+}
+
+// Form that decides amount of questions
+function selectQuestionAmount(e) {
+  e.preventDefault();
+  const questionAmount = getRadioValue();
+}
+
+startForm.addEventListener("click", () => {
+  radioContainers.forEach((radioEl) => {
+    // Remove Seleceted Label Styling
+    radioEl.classList.remove("selected-label");
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add("selected-label");
+    }
+  });
+});
+
+// Event Listeners
+startForm.addEventListener("submit", selectQuestionAmount);
